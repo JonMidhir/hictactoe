2ec05ff740b8e3bf1662361683b850f57074dde6
Jon Hope <john.hope@storyful.com>
2017-01-12 00:33:06 +0000

    Allow moves to be played and clean up types

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,38 +6,44 @@ import Data.List
 import Data.List.Split
 import Data.Char
 
-data Square = Square ((Int ,Int), Char) deriving (Show)
-data Board = Board [Square] deriving (Show)
-data BoardLine = BoardLine (Square, Square, Square) deriving (Show)
-
 -- Player Name Character
-data Player = Player String Char
+data Player = Player String Char deriving (Show)
+data Square = Square Position (Maybe Player) deriving (Show)
+
+type Position = (Int, Int)
+type Board = [Square]
+type BoardLine = (Square, Square, Square)
 
 -- The default board state in a new game
 initialBoard :: Board
-initialBoard = Board [Square ((0,0), ' '), Square ((0,1), ' '), Square ((0,2), ' '),
-                      Square ((1,0), ' '), Square ((1,1), ' '), Square ((1,2), ' '),
-                      Square ((2,0), ' '), Square ((2,1), ' '), Square ((2,2), ' ')]
+initialBoard = [Square (0,0) Nothing, Square (0,1) Nothing, Square (0,2) Nothing,
+                Square (1,0) Nothing, Square (1,1) Nothing, Square (1,2) Nothing,
+                Square (2,0) Nothing, Square (2,1) Nothing, Square (2,2) Nothing]
 
 -- Get the row from a square
 squareRow :: Square -> Int
-squareRow (Square ((a, _), _)) = a + 1
+squareRow (Square (a, _) _) = a + 1
 
 -- Get the value in a square
 squareValue :: Square -> Char
-squareValue (Square ((_, _), a)) = a
+squareValue (Square (_, _) Nothing) = ' '
+squareValue (Square (_, _) (Just (Player _ a))) = a
+
+-- Get the position (Int, Int) of a Square
+squarePosition :: Square -> Position
+squarePosition (Square position _) = position
 
 -- Return the lines in a given board. BoardLine type could be changed to use
 -- list of Squares instead of tuples to simplify this.
 boardLines :: Board -> [BoardLine]
-boardLines (Board (squares)) =
-  map BoardLine $ map (\(x:y:z:[]) -> (x, y, z)) $ chunksOf 3 squares
+boardLines = map (\(x:y:z:[]) -> (x, y, z)) . chunksOf 3
 
 -- Show a boardline as a string
 boardLineStr :: BoardLine -> String
-boardLineStr (BoardLine (a, b, c)) =
-  ((intToDigit rowNum):) . ("  " ++) . (intersperse ' ') . (intersperse '|') $ map squareValue [a, b, c]
-  where rowNum = squareRow a
+boardLineStr (a, b, c) =
+  (rowChar:) . ("  " ++) . (intersperse ' ') . (intersperse '|') $ squareValues
+  where rowChar = intToDigit $ squareRow a
+        squareValues = map squareValue [a, b, c]
 
 -- The gridspacer string for a standard board
 gridSpacer :: String
@@ -45,9 +51,20 @@ gridSpacer = "  ---+---+---"
 
 -- Presents a Board as a String
 boardStr :: Board -> String
-boardStr board = unlines $ ("   A   B   C ":) $ intersperse gridSpacer boardLineStrings
-  where boardLineStrings = map boardLineStr $ boardLines board
+boardStr board = unlines $ ("   A   B   C ":) $ intersperse gridSpacer lineStrings
+  where lineStrings = map boardLineStr $ boardLines board
+
+-- Play move if matching
+playMatchingMove :: Position -> Player -> Square -> Square
+playMatchingMove position player (Square position' _)
+  | position' == position = Square position' (Just player)
+  | otherwise             = Square position' Nothing
+
+-- Plays a given move by a player
+playMove :: Position -> Player -> Board -> Board
+playMove position player = map (playMatchingMove position player)
 
 initiateGame :: IO ()
 initiateGame = do
-  putStrLn $ boardStr initialBoard
+  let player1 = Player "John" 'x'
+  putStrLn $ boardStr $ playMove (1,2) player1 initialBoard
